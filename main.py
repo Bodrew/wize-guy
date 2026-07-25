4da9c115abdfa393e07f663dac51c31bf08b824f
Andrew <bodrew12@gmail.com>
2026-07-25 17:55:09 -0400

    Init tree

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ async def on_ready():
 
     # Create the whitelist application command.
 
-@client.tree.command(name="echo", description="Repeat your message bacl")
+@discord.app_commands.tree.command(name="echo", description="Repeat your message bacl")
 async def echo(interaction: discord.Interaction, message: str) -> None:
     await interaction.response.send_message(message)
 
