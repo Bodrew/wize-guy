f1240f7a77e026638fc67accb5d34ae3eef23b0f
bowena <bowena@theprovidenciagroup.com>
2026-08-10 10:08:07 -0400

    Fix unterminated string

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ async def on_message(message):
         #whitelist(user)
     
     if "!bedrock" in message.content:
-        response = "Welcome test!\n*If you\'re on Java, please send your username in chat so an admin can whitelist you!\n* If you\'re on Bedrock, let us know when you can join as the whitelist needs to be temporarily disabled to allow you to join. \n-# Thankfully, it\'s a one-time fix per player!
+        response = "Welcome test!\n*If you\'re on Java, please send your username in chat so an admin can whitelist you!\n* If you\'re on Bedrock, let us know when you can join as the whitelist needs to be temporarily disabled to allow you to join. \n-# Thankfully, it\'s a one-time fix per player!"
         await message.channel.send(response)
     
     if "!tps" in message.content:
